11081799e211a2d70ccad5eb386465fe3ad067b9
DRS22DownForce <syougui03@gmail.com>
2025-05-15 00:10:23 +0900

    Amplifyを使用した認証機能をAppコンポーネントに追加し、ユーザー名の表示とサインアウトボタンを実装しました。また、Amplify CLIの設定ファイルを新規作成し、関連する設定を追加しました。これにより、ユーザーエクスペリエンスが向上し、アプリケーションのセキュリティが強化されました。さらに、.gitignoreファイルを更新し、Amplify関連のファイルを無視する設定を追加しました。

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "frontend6f38f497": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
